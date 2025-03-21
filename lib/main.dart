1582380bfb965406f2c39a0e9c9175da0da25e3c
Toretay <abdymvzhit@gmail.com>
2025-03-21 21:14:31 +0500

    added ChangeNotifierProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/models/todos_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TodosModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
