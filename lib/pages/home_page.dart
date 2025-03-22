fee60d2760b842f9863819485ee7541f42929501
Toretay <abdymvzhit@gmail.com>
2025-03-22 14:44:46 +0500

    added focusNode to textfield

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,71 +11,80 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Todos")),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Consumer<TodosModel>(
-                  builder: (context, value, child) {
-                    List<String> todos = value.todos;
-                    if (todos.isEmpty) {
-                      return Center(child: Text("There are no todos yet."));
-                    } else {
-                      return ListView.builder(
-                        itemCount: todos.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            contentPadding: EdgeInsets.only(bottom: 16.0),
-                            title: Text(todos[index]),
-                            trailing: IconButton(
-                              onPressed: () {
-                                value.removeToDo(index);
-                              },
-                              icon: Icon(Icons.delete),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  },
+    return GestureDetector(
+      onTap: () {
+        _focusNode.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text("Todos")),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Consumer<TodosModel>(
+                    builder: (context, value, child) {
+                      List<String> todos = value.todos;
+                      if (todos.isEmpty) {
+                        return Center(child: Text("There are no todos yet."));
+                      } else {
+                        return ListView.builder(
+                          itemCount: todos.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              contentPadding: EdgeInsets.only(bottom: 16.0),
+                              title: Text(todos[index]),
+                              trailing: IconButton(
+                                onPressed: () {
+                                  value.removeToDo(index);
+                                },
+                                icon: Icon(Icons.delete),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      decoration: InputDecoration(border: OutlineInputBorder()),
+                SizedBox(height: 16.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        focusNode: _focusNode,
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Container(
-                    decoration: BoxDecoration(color: Colors.black),
-                    child: IconButton(
-                      onPressed: () {
-                        if (_controller.text.isNotEmpty) {
-                          Provider.of<TodosModel>(
-                            context,
-                            listen: false,
-                          ).addToDo(_controller.text);
-                          FocusScope.of(context).unfocus();
-                          _controller.clear();
-                        }
-                      },
-                      icon: Icon(Icons.add, color: Colors.white),
+                    SizedBox(width: 16.0),
+                    Container(
+                      decoration: BoxDecoration(color: Colors.black),
+                      child: IconButton(
+                        onPressed: () {
+                          if (_controller.text.isNotEmpty) {
+                            Provider.of<TodosModel>(
+                              context,
+                              listen: false,
+                            ).addToDo(_controller.text);
+                            _focusNode.unfocus();
+                            _controller.clear();
+                          }
+                        },
+                        icon: Icon(Icons.add, color: Colors.white),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
