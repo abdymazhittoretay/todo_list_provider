35236c346ddb7193f0b2c86dddc5da049c958a30
Toretay <abdymvzhit@gmail.com>
2025-03-26 16:39:53 +0500

    initialized the firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_list_provider/firebase_options.dart';
 import 'package:todo_list_provider/models/todos_model.dart';
 import 'package:todo_list_provider/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => TodosModel(),
